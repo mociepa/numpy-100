0ca32658c8939fa0f053ae46e041286dd02ddc6c
mociepa <michal.ociepa97@gmail.com>
2024-05-31 01:25:12 +0200

    New exercise done

--- exercises.py
+++ exercises.py
@@ -27,3 +27,19 @@ bb[: :-1]
 print(np.arange(0, 9).reshape(3, 3))
 
 z = np.nonzero([1,2,0,0,4,0])
+
+np.seterr(over = "warn")
+
+np.exp(1000)
+
+import matplotlib.pyplot as plt
+
+car = np.array(["Caterham", "Tesla", "Audi",  "BMW", "Ford", "Jeep"])
+weight = np.array([0.48, 1.7, 2, 2, 2.3, 3 ])
+
+# set colors and sizes
+colors = np.array([0, 1, 2, 3, 4, 5])
+sizes = np.array([20, 40, 60, 80, 100, 120])
+
+plt.scatter(car, weight, c=colors, s=sizes)
+plt.show()
